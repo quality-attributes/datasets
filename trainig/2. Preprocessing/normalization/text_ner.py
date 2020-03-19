9b5ac96188b2f112a42cde174762474e355c2cc6
Manolo Perez <manolope338@gmail.com>
2020-03-19 12:43:38 -0600

    NER tagging test

--- trainig/2. Preprocessing/normalization/text_ner.py
+++ trainig/2. Preprocessing/normalization/text_ner.py
@@ -8,7 +8,8 @@ unnecessary_tags = [
     "NUMBER",
     "ORGANIZATION",
     "COUNTRY",
-    "TIME"
+    "TIME",
+    "STATE_OR_PROVINCE"
 ]
 
 text = []
@@ -35,15 +36,21 @@ with open('../req_masked.json') as json_file:
     data = json.load(json_file)
 
 text = []
+sys_words = []
+usr_words = []
 
 for i in range(len(data)):
     req_text = data[i]['text']
     if data[i]['labels']:
         for tag in data[i]['labels']:
             if tag[2] == 'SYSTEM':
-                req_text = req_text.replace(req_text[tag[0]:tag[1]], 'system')
+                sys_words.append(req_text[tag[0]:tag[1]])
             if tag[2] == 'USER':
-                req_text = req_text.replace(req_text[tag[0]:tag[1]], 'user')
+                usr_words.append(req_text[tag[0]:tag[1]])
+        for word in sys_words:
+            req_text = req_text.replace(word, 'system')
+        for word in usr_words:
+            req_text = req_text.replace(word, 'user')
     text.append(' '.join(req_text.split()))
 
 with open('../normalized.txt', 'w') as f:
